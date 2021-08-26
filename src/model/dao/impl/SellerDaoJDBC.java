9c3e1abc45afe8dbf999a46eb332a48a214d8c50
welliston <welliston10@gmail.com>
2021-08-26 10:50:27 -0300

    update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -30,48 +30,70 @@ public class SellerDaoJDBC implements SellerDao{
 	
 	@Override
 	public void insert(Seller obj) {
+			PreparedStatement st = null;
+			try {
+				st = conn.prepareStatement(
+						         "INSERT INTO seller "
+						         + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+						         + "VALUES "
+						         + "(?, ?, ?, ?, ?)",
+						         Statement.RETURN_GENERATED_KEYS);
+				
+				st.setString(1, obj.getName());
+				st.setString(2, obj.getEmail());
+				st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+				st.setDouble(4, obj.getBaseSalary());
+				st.setInt(5, obj.getDepartment().getId());
+				
+				int rowsAffected = st.executeUpdate();
+				
+				if(rowsAffected > 0) {
+					ResultSet rs = st.getGeneratedKeys();
+					if(rs.next()) {
+						int id = rs.getInt(1);
+						obj.setId(id);
+					}
+					DB.closeResultSet(rs);
+				}
+				else {
+					throw new DbException("Unexpected error! No rows affected!");
+				}
+				
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+			finally {
+				DB.closeStatement(st);
+			}
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
 		PreparedStatement st = null;
 		try {
 			st = conn.prepareStatement(
-					"INSERT INTO seller "
-                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId) " 
-                    + "VALUES "
-                    + "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
+					"UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " 
+					+ "WHERE Id = ?");
 			
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
 			
-			int rowsAffected = st.executeUpdate();
+			st.executeUpdate();
 			
-			if(rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				if(rs.next()) {
-					int id = rs.getInt(1);
-					obj.setId(id);
-				}
-				DB.closeResultSet(rs);
-			}
-			else {
-				throw new DbException("Unexpected error! No rows affected!");
-			}
-			
-			}
+		}
 		catch(SQLException e) {
 			throw new DbException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
 		}
-		
-	}
-
-	@Override
-	public void update(Seller obj) {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -139,32 +161,29 @@ public class SellerDaoJDBC implements SellerDao{
 		ResultSet rs = null;
 		try {
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "ORDER BY Name");
+					  "SELECT seller.*,department.Name as DepName "
+					  + "FROM seller INNER JOIN department "
+					  + "ON seller.DepartmentId = department.Id "
+					  + "ORDER BY Name");
 			
-			
-			rs = st.executeQuery();
-			
-			List<Seller> list = new ArrayList<>();
-			Map<Integer, Department> map = new HashMap<>();
-			
-			while(rs.next()) {
-				
-				Department dep = map.get(rs.getInt("DepartmentId"));
-				
-				if(dep == null) {
-					dep = instantiateDepartment(rs);
-					map.put(rs.getInt("DepartmentId"), dep);
-				}
-				
-				Seller obj = instantiateSeller(rs,dep);
-				list.add(obj);
-			}
-			
-			return list;
-					
+		    rs = st.executeQuery();
+		    
+		    List<Seller> list = new ArrayList<>();
+		    Map<Integer, Department> map = new HashMap<>();
+		    
+		    while(rs.next()) {
+		    	
+		    	Department dep = map.get(rs.getInt("DepartmentId"));
+		    	
+		    	if(dep == null) {
+		    		dep = instantiateDepartment(rs);
+		    		map.put(rs.getInt("DepartmentId"), dep);
+		    	}
+		    	
+		    	Seller obj = instantiateSeller(rs, dep);
+		    	list.add(obj);
+		    }
+		    return list;
 		}
 		catch(SQLException e) {
 			throw new DbException(e.getMessage());
